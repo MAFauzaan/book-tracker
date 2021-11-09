e5cbba62f40a2b6349c8e2587dfd01e92522cf4b
MAFauzaan <m.adityofauzaan@gmail.com>
2021-11-09 16:49:16 +0700

    Remove saga

--- src/store/sagas/requests/books.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const getBooks = () => {
-   // return axios.get('https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=Ej5F7OLA4qM6oesIu3bWM2pOpdASZoHP')
-}
--- src/store/sagas/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { takeLatest } from 'redux-saga/effects'
-import { GET_BOOKS } from '../types'
-import { handleGetNotes } from './handler/books'
-
-export default function* rootSaga() {
-    yield takeLatest(GET_BOOKS, handleGetNotes)
-}
