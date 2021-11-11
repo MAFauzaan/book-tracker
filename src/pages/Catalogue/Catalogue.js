158cf94c75ffc0f833835b51a361a8f3f1c712cf
MAFauzaan <m.adityofauzaan@gmail.com>
2021-11-11 22:54:21 +0700

    Added a few lists

--- src/pages/Catalogue/Catalogue.js
+++ src/pages/Catalogue/Catalogue.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import SwiperCore, { Scrollbar } from "swiper";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 import { getBooks } from '../../store/actions/booksActions'
@@ -10,23 +12,17 @@ import CardUI from "../../components/card/Card";
 import NytLogo from "../../assets/nyt.svg"
 
 import 'swiper/swiper.scss'; 
+import "swiper/modules/scrollbar/scrollbar.scss"
 import "./Catalogue.scss"
 
+SwiperCore.use([Scrollbar])
 
 const Catalogue = () => {
 
     const dispatch = useDispatch();
     const books = useSelector(state => state.books.fetchedBooks);
 
-    const filteredHardCover = books.filter(({list_name}) => list_name === 'Hardcover Fiction');
-    const hardCoverFiction = filteredHardCover.slice(0,10);
-
-    const filteredYoungAdultHardcover = books.filter(({list_name}) => list_name === 'Young Adult Hardcover');
-    const youngAdultHardcover = filteredYoungAdultHardcover.slice(0,10);
-
-    const filteredBusinessBooks = books.filter(({list_name}) => list_name === 'Business Books');
-    const businessBooks = filteredBusinessBooks.slice(0,10);
-    
+    const displayItems = ['Hardcover Fiction', 'Young Adult Hardcover', 'Business Books'];    
 
     useEffect(() => {
         axios.get('https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=zf1MM73R7FJ2vPQgL25F00XEAbY4ZtJQ')
@@ -52,8 +48,6 @@ const Catalogue = () => {
         });
 
     }, [dispatch]);
-
-    console.log(hardCoverFiction)
     
     return (  
         <Container className="container">
@@ -63,34 +57,48 @@ const Catalogue = () => {
             </h1>
 
             <Grid className="booksSection">
-                <div className="bookSection__bookContainer">
-                    <div className="bookContainer__listName">
-                       
-                    </div>
-                    <Swiper 
-                        slidesPerView={4}
-                        spaceBetween={0}
-                        className="booksSection__hardcoverFiction"
-                    >
-                    {
-                        hardCoverFiction.map(book => {
-                            const bookCover = book.isbns[0].isbn10 || book.isbns[1].isbn10;
+                {
+                    displayItems.map(item => {
 
-                            return (
-                                <SwiperSlide>
-                                <CardUI 
-                                    key={bookCover}
-                                    name={book.book_details[0].title}
-                                    author={book.book_details[0].author}
-                                    src={`https://covers.openlibrary.org/b/isbn/${bookCover}-M.jpg`}
-                                />
-                                </SwiperSlide>
-                            )
-                        })
-                    }
-                    </Swiper>
-                </div>
+                        const listType = books.filter(({list_name}) => list_name === item).slice(0, 10)
+
+                        return (
+                            <div className="bookSection__bookContainer">
+                                <div className="bookContainer__listName">
+                                    <h1>{item}</h1>
+                                </div>
+                                <Swiper 
+                                    scrollbar={{"draggable": true, "hide": true}}
+                                    slidesPerView={4}
+                                    spaceBetween={0}
+                                    className="booksSection__hardcoverFiction"
+                                >
+                                {
+                                    listType.map(book => {
+                                        const bookCover = book.isbns[0].isbn10 || book.isbns[1].isbn10;
+            
+                                        return (
+                                            <SwiperSlide>
+                                            <CardUI 
+                                                key={bookCover}
+                                                name={book.book_details[0].title}
+                                                author={book.book_details[0].author}
+                                                src={`https://covers.openlibrary.org/b/isbn/${bookCover}-M.jpg`}
+                                            />
+                                            </SwiperSlide>
+                                        )
+                                    })
+                                }
+                                </Swiper>
+                        </div>
+                        )
+                    })
+                }
+              
             </Grid>
+            <Link to="/full" style={{textAlign: 'center', textDecoration: 'none'}}>
+                <h3>More on our full catalogue</h3>
+            </Link>
         </Container>
     );
 }
