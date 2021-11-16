4751d15d037d6baa84670e5c8664918b28d4bcc5
MAFauzaan <m.adityofauzaan@gmail.com>
2021-11-16 07:24:02 +0700

    Added library page

--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,6 +1,24 @@
+import { Container, Button } from "@mui/material";
+
+import "./Library.scss";
+
 const Library = () => {
+
+    const status = ['Reading List', 'Read', 'Finished'];
+
     return (  
-        <h1>Library</h1>
+        <Container>
+            <div>
+                <h1>Library</h1>
+                <div>
+                    {
+                        status.map((item, index) => (
+                            <Button key={index}>{item}</Button>
+                        ))
+                    }
+                </div>
+            </div>
+        </Container>
     );
 }
  
